c209ece2206add0930db2c1b7302d0eb732de560
TochilinMI <tochilin.m.i@mail.ru>
2022-09-18 23:44:40 +0300

    Enter value by console

--- src/ru/tochilinmi/Main.java
+++ src/ru/tochilinmi/Main.java
@@ -1,29 +1,13 @@
 package ru.tochilinmi;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int number = 10;
-        number = 11;
-        System.out.println("number: "+number);
-
-        Long long_num = 10000000L;
-        System.out.println("big_num: "+long_num);
-
-        float float_num = 5.76f;
-        System.out.println("float_num: "+float_num);
-
-        double double_num = 5.76156155d;
-        System.out.println("double_num: "+double_num);
-
-        boolean haveCar = true;
-        System.out.println("haveCar: "+haveCar);
-
-        char char_a = 'a';
-        System.out.println("char_a:"+char_a);
-
-        String str = "Hello world!";
-        System.out.println(str);
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a name");
+        String name = in.next();
+        System.out.println("Your name:"+name);
     }
 }
