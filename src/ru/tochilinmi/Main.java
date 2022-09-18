c0cfdb1c912924ebdf03e0ee0be9bfb8be12093f
TochilinMI <tochilin.m.i@mail.ru>
2022-09-18 23:38:29 +0300

    Types of variable

--- src/ru/tochilinmi/Main.java
+++ src/ru/tochilinmi/Main.java
@@ -3,6 +3,27 @@ package ru.tochilinmi;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world, from TochilinMI");
+        int number = 10;
+        number = 11;
+        System.out.println("number: "+number);
+
+        Long long_num = 10000000L;
+        System.out.println("big_num: "+long_num);
+
+        float float_num = 5.76f;
+        System.out.println("float_num: "+float_num);
+
+        double double_num = 5.76156155d;
+        System.out.println("double_num: "+double_num);
+
+        boolean haveCar = true;
+        System.out.println("haveCar: "+haveCar);
+
+        char char_a = 'a';
+        System.out.println("char_a:"+char_a);
+
+        String str = "Hello world!";
+        System.out.println(str);
+
     }
 }
