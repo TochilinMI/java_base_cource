92188d49dc67e03a73596f2eeb1fe64b7b012aa0
TochilinMI <tochilin.m.i@mail.ru>
2022-09-19 23:12:32 +0300

    Array

--- src/ru/tochilinmi/Main.java
+++ src/ru/tochilinmi/Main.java
@@ -1,16 +1,15 @@
 package ru.tochilinmi;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Put your num");
-        Scanner in = new Scanner(System.in);
-        Integer num = in.nextInt();
+        byte[] nums_array = new byte[5]; // create an array, allocated memory for 5th objects of array
+        byte nums_array2[] = new byte[6]; // another interpretation of initiate...
+        nums_array[0]=1;
+        nums_array2[0]=1;
 
-        int res = num < 10 ? (num + 10) : (num - 10);
-        System.out.println("Result:"+res);
-
+        String[] words = new String[]{"word1","word2","word3","word4"}; // Make array and fill it with auto-create memory
+        words[0] = "word5";
+        System.out.println(words[0]);
     }
 }
