b24d888703b377f364236da6416fc66910862454
TochilinMI <tochilin.m.i@mail.ru>
2022-09-19 23:18:09 +0300

    Array sort and copy

--- src/ru/tochilinmi/Main.java
+++ src/ru/tochilinmi/Main.java
@@ -1,15 +1,25 @@
 package ru.tochilinmi;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         byte[] nums_array = new byte[5]; // create an array, allocated memory for 5th objects of array
-        byte nums_array2[] = new byte[6]; // another interpretation of initiate...
         nums_array[0]=1;
-        nums_array2[0]=1;
+        nums_array[1]=10;
+        nums_array[2]=5;
+        nums_array[3]=105;
+        nums_array[4]=5;
+
+        byte[] new_array = Arrays.copyOf(nums_array,nums_array.length); //Copy paste array
+
 
         String[] words = new String[]{"word1","word2","word3","word4"}; // Make array and fill it with auto-create memory
         words[0] = "word5";
-        System.out.println(words[0]);
+
+        System.out.println(Arrays.toString(new_array));
+        Arrays.sort(new_array); //sorting an array
+        System.out.println(Arrays.toString(new_array));
     }
 }
