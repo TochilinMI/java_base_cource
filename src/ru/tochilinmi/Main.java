02dcdfc0d93cd6d1483c1c6618292df8d15a2b20
TochilinMI <tochilin.m.i@mail.ru>
2022-09-19 23:25:01 +0300

    many dimensions array 2D

--- src/ru/tochilinmi/Main.java
+++ src/ru/tochilinmi/Main.java
@@ -5,21 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        byte[] nums_array = new byte[5]; // create an array, allocated memory for 5th objects of array
-        nums_array[0]=1;
-        nums_array[1]=10;
-        nums_array[2]=5;
-        nums_array[3]=105;
-        nums_array[4]=5;
-
-        byte[] new_array = Arrays.copyOf(nums_array,nums_array.length); //Copy paste array
-
-
-        String[] words = new String[]{"word1","word2","word3","word4"}; // Make array and fill it with auto-create memory
-        words[0] = "word5";
-
-        System.out.println(Arrays.toString(new_array));
-        Arrays.sort(new_array); //sorting an array
-        System.out.println(Arrays.toString(new_array));
+        int[][] arr = new int[][]{ {5,6}, {3,2,1}, {1,2,3}, {2,3,5}}; // many dimensions array 2D
+        System.out.println(arr[0][1]);
     }
 }
