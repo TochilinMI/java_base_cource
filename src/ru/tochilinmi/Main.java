ac5421b07c1dfb944a229c66200ba7cc7467d039
TochilinMI <tochilin.m.i@mail.ru>
2022-09-20 22:14:28 +0300

    Rewrite file, read and add new text

--- src/ru/tochilinmi/Main.java
+++ src/ru/tochilinmi/Main.java
@@ -1,9 +1,6 @@
 package ru.tochilinmi;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -11,31 +8,37 @@ public class Main {
     public static void main(String[] args) {
 
         File file = new File("text.txt");
-
+        String context = reedFile();
         if(!file.exists()){
             try {
                 file.createNewFile();
             }catch (IOException e){
-                System.err.println("---------------------------------");
                 e.printStackTrace();
-                System.err.println("---------------------------------");
             }
         }
         try {
             System.out.println("Write some text");
             Scanner in = new Scanner(System.in);
             PrintWriter pw = new PrintWriter(file);
-
-                pw.println(in.nextLine());
+            context += reedFile() + in.nextLine();
+            pw.println(context);
 
             pw.close();
         } catch (FileNotFoundException e) {
-            System.err.println("---------------------------------");
             e.printStackTrace();
-            System.err.println("---------------------------------");
         }
-
-
-
+        System.out.print(reedFile());
+    }
+    public static String reedFile(){
+        BufferedReader br = null;
+        String line, result = "";
+        try {
+            br = new BufferedReader(new FileReader("text.txt"));
+            while ((line = br.readLine())!= null)result+=line+"\n";
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
