13ce97ce90f71d207a150a95d949dd47ce951d74
TochilinMI <tochilin.m.i@mail.ru>
2022-09-20 21:40:12 +0300

    Exception

--- src/ru/tochilinmi/Main.java
+++ src/ru/tochilinmi/Main.java
@@ -1,39 +1,19 @@
 package ru.tochilinmi;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        printSome(10,"th");
-        printSome(2);
-        printSome(sum(1,2));
+        float[] arr = new float[]{ 1.2f,23.1f,7.001f};
 
-        String[] words = new String[3];
-        makeArr(words);
-        makeArr(words);
-        makeArr(words);
-    }
-    // if returnless -> void
-    // else data type
-    public static String[] makeArr(String[] arr){   // Get array
-        Scanner in = new Scanner(System.in);
-        for (int i = 0; i < arr.length; i++){
-            System.out.print("Enter "+i+" element:");
-            arr[i] = in.nextLine();                 // push array
+        try {
+            System.out.println(arr[3]);
+            int i = 100/0;
+        } catch (Exception e){
+            System.err.println("--------------------------------------------------\nError: "+
+                                e+"\n--------------------------------------------------");
+        } finally {
+            System.out.println("Complete try and catch");
         }
-        return arr;                                 // return array
-    }
 
-    public static void printSome(int some, String at){
-        System.out.println("Hello "+some+at+" world!");
-    }
-    public static void printSome(int some){
-        System.out.println("Only data: "+some);
-    }
-
-    public static int sum(int a, int b){
-        return a+b;
     }
 }
